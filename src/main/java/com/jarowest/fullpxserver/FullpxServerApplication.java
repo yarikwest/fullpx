9d1f5a0d30ce8683e6e9fc98b550d17b040efb60
yarikwest <y.zakhidnyi@gmail.com>
2019-12-31 13:07:47 +0100

    Initial commit

---
+++ src/main/java/com/jarowest/fullpxserver/FullpxServerApplication.java
@@ -0,0 +1,13 @@
+package com.jarowest.fullpxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullpxServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullpxServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jarowest/fullpxserver/FullpxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarowest.fullpxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullpxServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
