e7bd9932fe8ab1166158691088d4afe5218a58e0
yarikwest <y.zakhidnyi@gmail.com>
2020-01-03 17:19:46 +0100

    Add global CORS

--- src/main/java/com/jarowest/fullpxserver/FullpxServerApplication.java
+++ src/main/java/com/jarowest/fullpxserver/FullpxServerApplication.java
@@ -2,6 +2,9 @@ package com.jarowest.fullpxserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FullpxServerApplication {
@@ -10,4 +13,13 @@ public class FullpxServerApplication {
         SpringApplication.run(FullpxServerApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
