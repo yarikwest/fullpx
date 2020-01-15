fd3da970e1c9410d5516c88259643529123eec27
yarikwest <y.zakhidnyi@gmail.com>
2020-01-15 11:56:42 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare var M: any;
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fullpx-client';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const options = {};
+
+    const elems = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(elems, options);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, Provider} from '@angular/core';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {BrowsePageComponent} from './browse-page/browse-page.component';
+import {SignUpPageComponent} from './cabinet/sign-up-page/sign-up-page.component';
+import {SharedModule} from './shared/shared.module';
+import {AuthInterceptor} from './shared/services/auth.interceptor';
+import {UserPageComponent} from './user-page/user-page.component';
+import {UserPageAboutComponent} from './user-page-about/user-page-about.component';
+import {UserPageAlbumsComponent} from './user-page-albums/user-page-albums.component';
+import {UserPageCommentsComponent} from './user-page-comments/user-page-comments.component';
+import { AlbumCardComponent } from './album-card/album-card.component';
+import { AlbumComponent } from './album/album.component';
+import { CommentComponent } from './comment/comment.component';
+
+const INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  multi: true,
+  useClass: AuthInterceptor
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
   ],
-  providers: [],
+  exports: [],
+  providers: [INTERCEPTOR_PROVIDER],
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    BrowsePageComponent,
+    SignUpPageComponent,
+    UserPageComponent,
+    UserPageAboutComponent,
+    UserPageAlbumsComponent,
+    UserPageCommentsComponent,
+    AlbumCardComponent,
+    AlbumComponent,
+    CommentComponent,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
