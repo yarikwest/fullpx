b196534c4bf390c2b58d05950faf7e528d11c7ad
yarikwest <y.zakhidnyi@gmail.com>
2020-01-16 16:40:38 +0100

    Add gallery in dashboard

--- src/app/cabinet/dashboard-page/dashboard-page.component.ts
+++ src/app/cabinet/dashboard-page/dashboard-page.component.ts
@@ -1,7 +1,8 @@
 import {Component, DoCheck, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import {User} from '../../shared/interfaces';
+import {Photo, User} from '../../shared/interfaces';
 import {UserService} from '../../shared/services/user.service';
 import {CommunicateService} from '../../shared/services/communicate.service';
+import {PhotoService} from '../../shared/services/photo.service';
 
 declare var M: any;
 
@@ -13,22 +14,26 @@ declare var M: any;
 export class DashboardPageComponent implements OnInit, DoCheck {
 
   user: User;
+  photos: Photo[];
 
   constructor(
     private userService: UserService,
+    private photoService: PhotoService,
     private communicateService: CommunicateService
   ) {
   }
 
   ngOnInit(): void {
-    M.AutoInit();
     this.userService.getUser(localStorage.getItem('username')).subscribe(user => this.user = user);
+    this.photoService.getPhotosByUsername(localStorage.getItem('username')).subscribe(photos => this.photos = photos);
     this.communicateService.setUser(this.user);
+    this.communicateService.setPhotos(this.photos);
+    M.AutoInit();
   }
 
   ngDoCheck(): void {
     this.communicateService.setUser(this.user);
+    this.communicateService.setPhotos(this.photos);
   }
-
 
 }
