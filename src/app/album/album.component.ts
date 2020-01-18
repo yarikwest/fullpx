8eb3e6db71be51747d8847cc7d39f18d0a03ce9e
yarikwest <y.zakhidnyi@gmail.com>
2020-01-18 16:14:51 +0100

    Some refactoring

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -4,25 +4,25 @@ import {Album} from '../shared/interfaces';
 import {AlbumService} from '../shared/services/album.service';
 
 @Component({
-  selector: 'app-album-page',
-  templateUrl: './album.component.html',
-  styleUrls: ['./album.component.css']
+    selector: 'app-album-page',
+    templateUrl: './album.component.html',
+    styleUrls: ['./album.component.css']
 })
 export class AlbumComponent implements OnInit {
-  album: Album;
-  albumNameParam: string;
+    album: Album;
+    albumNameParam: string;
 
-  constructor(
-    private route: ActivatedRoute,
-    private albumService: AlbumService,
-  ) {
-  }
+    constructor(
+        private route: ActivatedRoute,
+        private albumService: AlbumService,
+    ) {
+    }
 
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.albumNameParam = params.album;
-      this.albumService.getAlbum(localStorage.getItem('username'), this.albumNameParam).subscribe(album => this.album = album);
-    });
-  }
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.albumNameParam = params.album;
+            this.albumService.getAlbum(localStorage.getItem('username'), this.albumNameParam).subscribe(album => this.album = album);
+        });
+    }
 
 }
